ce6347eebcb029d7e736555c3dfbb48419e47f14
Elie Mélois <elie@lumapps.com>
2013-10-24 11:56:21 +0200

    Update the decorator to accept all arguments

--- DJANGO_GAE/authorized_decorator.py
+++ DJANGO_GAE/authorized_decorator.py
@@ -4,9 +4,9 @@ from google.appengine.ext import webapp
 
 
 def authorized(f):
-    def decorated_call(self):
+    def decorated_call(self, *callback_args, **callback_kwargs):
         if "@lumapps.com" in users.get_current_user().email() or "@google.com" in users.get_current_user().email() or "@oxylane.com" in users.get_current_user().email() or "@gpartner.eu" in users.get_current_user().email():
-            return f(self)
+            return f(self, *callback_args, **callback_kwargs)
         else:
             webapp.abort(401)
 
