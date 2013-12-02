7a540edf4fc1c58bd8065047980848689303c4b6
Luca Cioria <vircheck@gmail.com>
2013-12-02 17:25:28 +0100

    fix decorator to return 401 and work with Django and to work if user was not logged in

--- DJANGO_GAE/authorized_decorator.py
+++ DJANGO_GAE/authorized_decorator.py
@@ -1,13 +1,19 @@
 import logging
 from google.appengine.api import users
-from google.appengine.ext import webapp
-
+from django.http import HttpResponse
 
 def authorized(f):
     def decorated_call(self, *callback_args, **callback_kwargs):
-        if "@lumapps.com" in users.get_current_user().email() or "@google.com" in users.get_current_user().email() or "@oxylane.com" in users.get_current_user().email() or "@gpartner.eu" in users.get_current_user().email():
+    	user = users.get_current_user()
+    	# check if logged in
+    	if not user:
+            return HttpResponse(status=401)
+        else:
+        	email = user.email()
+    	# check if authorized
+        if "@lumapps.com" in email or "@google.com" in email or "@oxylane.com" in email or "@gpartner.eu" in email:
             return f(self, *callback_args, **callback_kwargs)
         else:
-            webapp.abort(401)
+            return HttpResponse(status=401)
 
     return decorated_call
